f2b1964af348068e80afd9d0eca824049561c668
sarix511 <nazmatahir1978@gmail.com>
2026-03-05 16:55:01 +0500

    Refactor comments and improve text sizing logic

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests, io
 
 app = FastAPI()
 
-# Allow all origins for browser / JS
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,7 +17,7 @@ app.add_middleware(
 @app.get("/generate")
 async def generate(guild_name: str = Query(...)):
     try:
-        # Real background image
+        # Background image
         img_url = "https://cdn.designfast.io/image/2026-03-05/8bb255db-96bc-4566-bdf4-d83815067a96.jpeg"
         resp = requests.get(img_url)
         resp.raise_for_status()
@@ -34,19 +34,36 @@ async def generate(guild_name: str = Query(...)):
         except:
             font = ImageFont.load_default()
 
-        # Dynamic font size
+        # Dynamic font size using draw.textbbox
         max_font_size = 250
-        if isinstance(font, ImageFont.FreeTypeFont):
-            while font.getsize(guild_name)[0] < max_width and font_size < max_font_size:
-                font_size += 5
+        while True:
+            if isinstance(font, ImageFont.FreeTypeFont):
+                bbox = draw.textbbox((0,0), guild_name, font=font)
+                text_width = bbox[2] - bbox[0]
+            else:
+                text_width, _ = draw.textsize(guild_name, font=font)
+            
+            if text_width >= max_width or font_size >= max_font_size:
+                break
+            
+            font_size += 5
+            try:
                 font = ImageFont.truetype(font_path, font_size)
+            except:
+                break
 
-        # Text position (adjust to blank area)
-        text_width, text_height = draw.textsize(guild_name, font=font)
+        # Position (adjust vertical)
+        if isinstance(font, ImageFont.FreeTypeFont):
+            bbox = draw.textbbox((0,0), guild_name, font=font)
+            text_width = bbox[2] - bbox[0]
+            text_height = bbox[3] - bbox[1]
+        else:
+            text_width, text_height = draw.textsize(guild_name, font=font)
+
         x = (img.width - text_width) // 2
-        y = 180  # adjust vertical position according to blank space
+        y = 180  # adjust vertical position to blank area
 
-        # Black outline
+        # Outline
         outline_range = 3
         for ox in range(-outline_range, outline_range+1):
             for oy in range(-outline_range, outline_range+1):
